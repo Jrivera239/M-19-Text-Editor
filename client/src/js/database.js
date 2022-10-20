cca7e437478b75a64f215c25053c594d73fc0100
Joel Rivera <106676739+Jrivera239@users.noreply.github.com>
2022-10-20 18:17:59 -0400

    database

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,4 +1,5 @@
 import { openDB } from 'idb';
+import 'regenerator-runtime/runtime';
 
 const initdb = async () =>
   openDB('jate', 1, {
@@ -10,12 +11,32 @@ const initdb = async () =>
       db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
       console.log('jate database created');
     },
-  });
+  }
+);
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+
+  console.log('Put to the database');
+  const contactDb = await openDB('jate_db', 1);
+  const tx = contactDb.transaction('jate_db', 'readwrite');
+  const store = tx.objectStore('jate');
+  
+  const request = store.put({content: content });
+  const result = await request;
+  console.log('🚀 - data saved to the database', result);
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+
+export const getDb = async () => {
+  console.log('GET from the database');
+  
+const contactDb = await openDB('jate_db', 1);
+const tx = contactDb.transaction('jate_db', 'readonly');
+const store = tx.objectStore('jate');
+const request = store.getAll();
+const result = await request;
+console.log('result.value', result);
+return result;
+};
 
 initdb();
